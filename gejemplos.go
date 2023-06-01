98693d0b1d314f8ec6ace368e72050060c716b47
Samuel Rodriguez Sevilla <samuel.rodriguez.sevilla@ericsson.com>
2023-06-01 11:00:35 +0200

    Adding a package

--- gejemplos.go
+++ gejemplos.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/laparca/gejemplos/sumar"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	n, m := 1, 2
+
+	fmt.Println(sumar.Suma(n, m))
 }
---
+++ sumar/sumar.go
@@ -0,0 +1,6 @@
+package sumar
+
+// Suma add two integers
+func Suma(a int, b int) int {
+	return a + b
+}
